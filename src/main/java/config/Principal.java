e7ec5bbca21cc4bf5152700b97bfc10c21bbb3d3
Jacob <blopezjacob@gmail.com>
2025-12-15 11:02:02 -0500

    Update Principal.java

--- src/main/java/config/Principal.java
+++ src/main/java/config/Principal.java
@@ -4,8 +4,7 @@ public class Principal {
 
     public static void main(String[] args) {
         
-        // Como 'Conexion' y 'Principal' están en la misma carpeta (config),
-        // NO hace falta hacer "import". Se saludan directamente.
+        
         
         Conexion miConexion = new Conexion();
         miConexion.getConexion();
